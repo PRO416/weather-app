31019f3f97b27fc1d66c7b8e7904b6adb1f6321b
PRO416 <thapelomataboge18@gmail.com>
2020-08-09 03:16:53 +0200

    created a boiler plate for my react environment
    Using create-react-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Weather Update</h3>
+      <h3>Is It fine now?</h3>
+    </div>
+  );
+}
+
+export default App;
